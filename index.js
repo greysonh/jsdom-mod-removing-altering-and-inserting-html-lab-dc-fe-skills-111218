3c7c13e7f13df2b68650e0e45f9835eaf37aaadc
Greyson Harris <gharris@esri.com>
2018-12-11 02:02:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 /* Enter the code to remove the main node element under this comment */
+let element = document.getElementById('main');
+element.remove();
 
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
